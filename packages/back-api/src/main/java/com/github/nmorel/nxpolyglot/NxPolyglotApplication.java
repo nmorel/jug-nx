d95635e5cbb2d5065aa05282c20fcdfe95d7bda9
Nicolas Morel <nmr.morel@gmail.com>
2023-01-08 23:14:56 +0100

    Add auto reload

--- packages/back-api/src/main/java/com/github/nmorel/nxpolyglot/NxPolyglotApplication.java
+++ packages/back-api/src/main/java/com/github/nmorel/nxpolyglot/NxPolyglotApplication.java
@@ -8,6 +8,7 @@ public class NxPolyglotApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NxPolyglotApplication.class, args);
+		System.out.println("aaaaa");
 	}
 
 }
