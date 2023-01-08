5c84ac0b05288df1c95a83dca6fe4e30146a9ccd
Nicolas Morel <nmr.morel@gmail.com>
2023-01-08 22:37:10 +0100

    Add a spring boot api package

---
+++ packages/back-api/src/main/java/com/github/nmorel/nxpolyglot/NxPolyglotApplication.java
@@ -0,0 +1,13 @@
+package com.github.nmorel.nxpolyglot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NxPolyglotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NxPolyglotApplication.class, args);
+	}
+
+}
---
+++ packages/back-api/src/test/java/com/github/nmorel/nxpolyglot/NxPolyglotApplicationTests.java
@@ -0,0 +1,14 @@
+package com.github.nmorel.nxpolyglot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NxPolyglotApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("BB");
+	}
+
+}
