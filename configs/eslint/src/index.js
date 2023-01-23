8c794df707b420cab3e8cdce246d40d8569e1ee4
Nicolas Morel <nmr.morel@gmail.com>
2023-01-23 22:18:59 +0100

    Move config package to configs folder and add lint target

--- configs/eslint/src/index.js
+++ configs/eslint/src/index.js
@@ -8,7 +8,7 @@ module.exports = {
   plugins: ['@typescript-eslint', 'prettier'],
   rules: {
     '@typescript-eslint/ban-ts-comment': 'off',
-    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/no-unused-vars': 'error',
     '@typescript-eslint/no-explicit-any': 'off',
     'prettier/prettier': ['error', require('prettier-config-custom')],
   },
