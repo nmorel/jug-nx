6e3007bb5d194b080d22943c0028125dd2233ae3
Nicolas Morel <nmr.morel@gmail.com>
2023-01-24 12:59:52 +0100

    Fix lint on bundle.mjs

--- bundle/src/bundle.mjs
+++ bundle/src/bundle.mjs
@@ -1,31 +1,31 @@
 #!/usr/bin/env zx
 
-import "zx/globals";
-import path from "node:path";
-import { XMLParser } from "fast-xml-parser";
-import { readFile } from "fs/promises";
+import 'zx/globals'
+import path from 'node:path'
+import {XMLParser} from 'fast-xml-parser'
+import {readFile} from 'fs/promises'
 
-const apiFolder = path.join(__dirname, "../../backend/api");
+const apiFolder = path.join(__dirname, '../../backend/api')
 
 // Api informations
-const pomXmlData = await readFile(path.join(apiFolder, "pom.xml"));
-const parser = new XMLParser();
+const pomXmlData = await readFile(path.join(apiFolder, 'pom.xml'))
+const parser = new XMLParser()
 const {
-  project: { artifactId, version, packaging },
-} = parser.parse(pomXmlData);
+  project: {artifactId, version, packaging},
+} = parser.parse(pomXmlData)
 
-const apiArtifact = path.join(apiFolder, "target", `${artifactId}-${version}.${packaging}`);
+const apiArtifact = path.join(apiFolder, 'target', `${artifactId}-${version}.${packaging}`)
 
 // Webapp informations
-const webappFolder = path.join(__dirname, "../../frontend/app/dist");
+const webappFolder = path.join(__dirname, '../../frontend/app/dist')
 
 // Bundle informations
-const buildFolder = path.join(__dirname, "../dist");
-const targetArtifact = `${buildFolder}/nx-polyglot.${packaging}`;
+const buildFolder = path.join(__dirname, '../dist')
+const targetArtifact = `${buildFolder}/nx-polyglot.${packaging}`
 
-await $`rm -rf ${buildFolder}/*`;
-await $`mkdir -p ${buildFolder}/WEB-INF/classes/static`;
-await $`cp ${apiArtifact} ${targetArtifact}`;
-await $`cp -r ${webappFolder}/* ${buildFolder}/WEB-INF/classes/static/`;
-await $`jar uf ${targetArtifact} -C ${buildFolder}/ WEB-INF/`;
-await $`rm -rf ./${buildFolder}/WEB-INF`;
+await $`rm -rf ${buildFolder}/*`
+await $`mkdir -p ${buildFolder}/WEB-INF/classes/static`
+await $`cp ${apiArtifact} ${targetArtifact}`
+await $`cp -r ${webappFolder}/* ${buildFolder}/WEB-INF/classes/static/`
+await $`jar uf ${targetArtifact} -C ${buildFolder}/ WEB-INF/`
+await $`rm -rf ./${buildFolder}/WEB-INF`
